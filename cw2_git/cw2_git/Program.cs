5ecd0a030d737f02b5be9f5968850b511b8a8e14
Magda Wójcik <s33101@pjwstk.edu.pl>
2026-03-14 13:44:52 +0100

    added new function CalculateAverage

--- cw2_git/cw2_git/Program.cs
+++ cw2_git/cw2_git/Program.cs
@@ -1,1 +1,15 @@
 ﻿Console.WriteLine("Hello, World!");
+
+double CalculateAverage(int[] values)
+{
+    int sum = 0;
+
+    foreach (int el in values)
+    {
+        sum += el;
+    }
+    
+    return sum / values.Length;
+}
+
+Console.WriteLine(CalculateAverage(new int[] { 1, 2, 3, 4, 5 }));
