a66b142783609f5df76f979ea9873f4e830e150c
Magda Wójcik <s33101@pjwstk.edu.pl>
2026-03-14 13:46:47 +0100

    modified Program.cs file - added more tests

--- cw2_git/cw2_git/Program.cs
+++ cw2_git/cw2_git/Program.cs
@@ -13,3 +13,5 @@ double CalculateAverage(int[] values)
 }
 
 Console.WriteLine(CalculateAverage(new int[] { 1, 2, 3, 4, 5 }));
+Console.WriteLine(CalculateAverage(new int[] { 6,8,9 }));
+Console.WriteLine(CalculateAverage(new int[] { 11,11,23}));
