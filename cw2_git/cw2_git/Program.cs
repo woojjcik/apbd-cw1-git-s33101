4b0cdfdb03494f0aa5ef6ebf870bf575d322171c
Magda Wójcik <s33101@pjwstk.edu.pl>
2026-03-14 14:31:49 +0100

    mofified file Program.cs - added CalculateMax function

--- cw2_git/cw2_git/Program.cs
+++ cw2_git/cw2_git/Program.cs
@@ -15,3 +15,18 @@ double CalculateAverage(int[] values)
 Console.WriteLine(CalculateAverage(new int[] { 1, 2, 3, 4, 5 }));
 Console.WriteLine(CalculateAverage(new int[] { 6,8,9 }));
 Console.WriteLine(CalculateAverage(new int[] { 11,11,23}));
+
+int CalculateMac(int[] values)
+{
+    int max = values[0];
+
+    foreach (int i in values)
+    {
+        if (i > max)
+        {
+            max = i;
+        }
+    }
+    
+    return max;
+}
